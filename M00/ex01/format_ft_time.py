ec8eb716ee29e45b9f2567da4f5bdf8e57c85fe5
Astrid GAULTIER <asgaulti@bess-f3r3s9.clusters.42paris.fr>
2023-07-06 13:44:47 +0200

    ex01 et 2 en cours

--- M00/ex01/format_ft_time.py
+++ M00/ex01/format_ft_time.py
@@ -1,4 +1,31 @@
-import time
+import time, datetime
+
+months = {
+    1 : 'Jan',
+    2 : 'Feb',
+    3 : 'Mar',
+    4 : 'Apr',
+    5 : 'May',
+    6 : 'Jun',
+    7 : 'Jul',
+}
+
 
 seconds = time.time()
-print("Seconds since January 1, 1970:", {seconds}, "or", {seconds:.2})
+len_s = len(seconds)
+
+
+other_seconds = '{:.2e}'.format(time.time())
+myDate = datetime.datetime.today()
+month = myDate.month
+
+
+print("Seconds since January 1, 1970:", seconds, "or", other_seconds, "in sientific notation")
+print(months[month], myDate.day, myDate.year)
+
+
+# partir de la fin et tous les 3 chiffres, mettre un sep=','
+# ou
+# diviser et modulo le nb de chiffres par 3 ;
+# le modulo me dit ou mettre la 1ere virgule
+# ensuite ous les 3 chiffres je mets une virgule avec un sep 
