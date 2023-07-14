d68bc954f1b0f83150426455590b062e72d5d7b2
Astrid GAULTIER <asgaulti@paul-f3Br3s3.clusters.42paris.fr>
2023-07-14 18:22:55 +0200

    M01 ex00 mise en place

---
+++ M01/ex00/give_bmi.py
@@ -0,0 +1,28 @@
+import numpy
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+
+    """
+    This function takes 2 lists of integers or floats in input and returns
+    a list of BMI values.
+    """
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+
+    """
+    This function accepts a list of integers or floats and an integer representing
+    a limit as parameters. It returns a list of booleans (True if above the limit).
+    """
+
+
+def main():
+    if (len(sys.argv) != 1)
+        assert False, "ERROR: wrong number of arguments."
+    height = sys.argv[1]
+    w = sys.argv[2]
+
+
+if __name__ == "__main__":
+    main()
