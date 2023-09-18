0a0c7ec7cae605688f2a823b4cc2f5d289b5bc92
Astrid GAULTIER <asgaulti@bess-f1r1s1.clusters.42paris.fr>
2023-09-18 14:53:52 +0200

    M00 reprise avant validation ex00 ok

--- M00/ex00/Hello.py
+++ M00/ex00/Hello.py
@@ -5,7 +5,8 @@ ft_dict = {"Hello": "titi!"}
 
 ft_list[1] = "World!"
 ft_tuple = ("Hello", "France!")
-ft_set = {"Hello", "Paris!"}
+ft_set.discard('tutu!')
+ft_set.add("Paris!")
 ft_dict = {"Hello": "42Paris!"}
 
 print(ft_list)
