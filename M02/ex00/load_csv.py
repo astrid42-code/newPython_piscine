ac0cbf02aa16416a6add59e4b5b7383237adbc7b
Astrid GAULTIER <asgaulti@bess-f2r1s1.clusters.42paris.fr>
2023-11-26 16:51:09 +0100

    M01 norme ok (sauf commentaires a retirer dans version finale) + prep M02 ex00

---
+++ M02/ex00/load_csv.py
@@ -0,0 +1,9 @@
+def load(path: str) -> Dataset:
+# (You have to adapt the type of return according to your library)
+
+    '''
+    This function takes a path as argument, writes the dimensions
+    of the data set and returns it
+    '''
+
+    
