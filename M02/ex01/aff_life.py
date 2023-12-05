f295c3f3296b306bc91f337f883014cae6d89a01
Astrid GAULTIER <asgaulti@bess-f1r1s1.clusters.42paris.fr>
2023-12-05 13:48:52 +0100

    M02 - ex01 en cours2

--- M02/ex01/aff_life.py
+++ M02/ex01/aff_life.py
@@ -16,10 +16,51 @@ def aff_life(graph, country): # quelle valeur de return?
     Your graph must have a title and a legend for each axis.
     '''
 
-    df = graph.loc[graph['country'] == country] # = ligne pour la France (mais aussi le header?)
-    # print(df.iloc[0][0]) > France
+    df = graph['country'] == country
+    name = graph['country'].values[0]
+    life_expect = df.values[0]
+
+    res = plt.plot([life_expect], [1])
+    plt.xlabel('Year')
+    plt.ylabel('Life expectancy')
+    plt.show()
     
+    
+    
+#     df = graph.loc[graph['country'] == country] # = ligne pour la France (mais aussi le header?)
 
-# https://python.doctor/page-creer-graphiques-scientifiques-python-apprendre
 
-# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplots.html
+
+#     # print(graph['country'])
+
+#     # print(type(graph['country'])) # > type = series, ie les colonnes d'un dataframe
+#     # un data frame a 2 dimensions avec plusieurs colonnes, alors que la Series n’a qu’une seule dimension. 
+#     # Une Series ne peut contenir qu’un seul type, alors qu’un data frame, qui est finalement une collection de Series, peut contenir des colonnes de types différents
+#     # https://openclassrooms.com/fr/courses/7771531-decouvrez-les-librairies-python-pour-la-data-science/7857439-manipulez-le-data-frame
+
+#     # print(df.iloc[0]) # > France et sa data
+#     # print(graph.sort_values('country'))
+#     # print(df.iloc[0,1:])
+
+#     # print(df.iloc[0:,0:])
+
+
+#     # print(df.iloc[0][0]) # > France
+    
+#     x = df
+
+#     x = df.iloc[0,1:]
+#     y = df.loc() #df.index > donne le nbr total de lignes dans le doc
+#     # print(y) # la ligne de titre : mais comment la recup?
+#     plt.plot(x, df)
+#     plt.xlabel('Year')
+#     plt.ylabel('Life expectancy')
+#     plt.axis([1800, 2080, 30, 80])
+#     plt.show()
+
+# # https://python.doctor/page-creer-graphiques-scientifiques-python-apprendre
+
+# # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplots.html
+
+# # doc generale sur graphiques (openclassroom):
+# # https://openclassrooms.com/fr/courses/7771531-decouvrez-les-librairies-python-pour-la-data-science/7858074-maitrisez-les-bonnes-pratiques-de-la-data-visualisation
--- M02/ex01/load_csv.py
+++ M02/ex01/load_csv.py
@@ -23,7 +23,7 @@ def load(path: str) -> pd.DataFrame:
     # The shape attribute returns a two-item tuple of the number of rows and the number of columns in the DataFrame. For a Series, it returns a one-item tuple
     print("Loading dataset of dimensions", df.shape)
     # print(df)
-    print(type(df))
+    # print(type(df))
 
     return (df)
     # doit on close?
