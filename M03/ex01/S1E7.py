47db0fc8c7ce4b3fbf28f91b70a6f02f318bda42
Astrid GAULTIER <asgaulti@bess-f1r5s3.clusters.42paris.fr>
2023-12-10 14:46:10 +0100

    M03 ex01 3 (bound method a faire)

--- M03/ex01/S1E7.py
+++ M03/ex01/S1E7.py
@@ -3,27 +3,63 @@ from S1E9 import Character
 
 class Baratheon(Character):
     '''
-    Baratheon family which inherits from Character class
+Representing the Baratheon family.
     '''
-    #your code here
+
+    def __init__(self, name, is_alive=True):
+        '''
+        Init Stark's class
+        '''
+        # try:
+        #     Character.__init__(self, name, is_alive)
+        # except Exception as e:
+        #     print(e)
+        Character.__init__(self, name, is_alive)
+
+    def die(self):
+        '''
+        method that passes is_alive from True to False
+        '''
+        # try:
+        #     self.is_alive = not self.is_alive
+        # except Exception as e:
+        #     print(e)
+        self.is_alive = False
 
 
 class Lannister(Character):
     '''
-    Lannister family which inherits from Character class
+Representing the Lannister family.
     '''
-    #your code here
-
-
-        # decorator
-    def create_lannister(your code here):
+    def __init__(self, name, is_alive=True):
         '''
-        Method to create characters in a chain
+        Init Stark's class
         '''
-        #your code here
+        # try:
+        #     Character.__init__(self, name, is_alive)
+        # except Exception as e:
+        #     print(e)
+        Character.__init__(self, name, is_alive)
+
+    def die(self):
+        '''
+        method that passes is_alive from True to False
+        '''
+        # try:
+        #     self.is_alive = not self.is_alive
+        # except Exception as e:
+        #     print(e)
+        self.is_alive = False
+
+    #     # decorator
+    # def create_lannister(your code here):
+    #     '''
+    #     Method to create characters in a chain
+    #     '''
+    #     #your code here
 
 
-__str__ et __repr__ :
-The __str__() and __repr__() methods can be helpful in debugging Python code by logging or printing useful information about an object
-https://www.digitalocean.com/community/tutorials/python-str-repr-functions
-https://www.docstring.fr/glossaire/str/
+# __str__ et __repr__ :
+# The __str__() and __repr__() methods can be helpful in debugging Python code by logging or printing useful information about an object
+# https://www.digitalocean.com/community/tutorials/python-str-repr-functions
+# https://www.docstring.fr/glossaire/str/
--- M03/ex01/S1E9.py
+++ M03/ex01/S1E9.py
@@ -3,53 +3,54 @@ from abc import ABC, abstractmethod
 
 class Character(ABC):
     """
-    This class takes a first_name as first parameter,
-    is_alive as second non mandatory parameter set to True by default
-    and can change the health state of the character
-    with a method that passes is_alive from True to False
+This class takes a first_name as first parameter,
+is_alive as second non mandatory parameter set to True by default
+and can change the health state of the character
+with a method that passes is_alive from True to False
     """
 
     def __init__(self, name, is_alive=True):
         '''
-        Init Character's class
+Init Character's class
         '''
 
         self.name = name
         self.is_alive = is_alive
-        
+
     @abstractmethod
     def die(self):
         '''
-        method checking if is the character alive
+method checking if is the character alive
         '''
         pass
 
 
 class Stark(Character):
     """
-    "stark" class inherits from Character's class
+"stark" class inherits from Character's class
     """
 
     def __init__(self, name, is_alive=True):
         '''
-        Init Stark's class
+Init Stark's class
         '''
         # try:
         #     Character.__init__(self, name, is_alive)
         # except Exception as e:
         #     print(e)
         Character.__init__(self, name, is_alive)
+        self.family_name = "Baratheon"
 
     def die(self):
         '''
-        method that passes is_alive from True to False
+method that passes is_alive from True to False
         '''
         # try:
-        #     self.is_alive = not self.is_alive
+        #     self.is_alive = False
         # except Exception as e:
         #     print(e)
         self.is_alive = False
-        
+
 
 # https://irma.math.unistra.fr/~franck/cours/Pythonl2/cours7_2021_slides.pdf
 # Une classe abstraite est une classe qui ne permet pas d’instancier des objets.
