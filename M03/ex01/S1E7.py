004642edc084bc039febfacc6a98060091453af9
Astrid GAULTIER <asgaulti@bess-f1r5s3.clusters.42paris.fr>
2023-12-10 16:44:21 +0100

    M03 ex01 ok

--- M03/ex01/S1E7.py
+++ M03/ex01/S1E7.py
@@ -2,64 +2,67 @@ from S1E9 import Character
 
 
 class Baratheon(Character):
-    '''
-Representing the Baratheon family.
-    '''
+    '''Representing the Baratheon family.'''
 
     def __init__(self, name, is_alive=True):
-        '''
-        Init Stark's class
-        '''
-        # try:
-        #     Character.__init__(self, name, is_alive)
-        # except Exception as e:
-        #     print(e)
+        '''Init Stark's class'''
+
         Character.__init__(self, name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def __str__(self):
+        return f'Vector: (\'{self.family_name}\', {self.eyes}\',{self.hairs})'
+
+    def __repr__(self):
+        return f'Vector: (\'{self.family_name}\', {self.eyes}\',{self.hairs})'
+        # return self.__str__()
 
     def die(self):
-        '''
-        method that passes is_alive from True to False
-        '''
-        # try:
-        #     self.is_alive = not self.is_alive
-        # except Exception as e:
-        #     print(e)
+        '''method that passes is_alive from True to False'''
+
         self.is_alive = False
 
 
 class Lannister(Character):
-    '''
-Representing the Lannister family.
-    '''
+    '''Representing the Lannister family.'''
+
     def __init__(self, name, is_alive=True):
-        '''
-        Init Stark's class
-        '''
-        # try:
-        #     Character.__init__(self, name, is_alive)
-        # except Exception as e:
-        #     print(e)
+        '''Init Stark's class'''
+
         Character.__init__(self, name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def __str__(self):
+        return f'Vector: (\'{self.family_name}\', {self.eyes}\',{self.hairs})'
+
+    def __repr__(self):
+        return f'Vector: (\'{self.family_name}\', {self.eyes}\',{self.hairs})'
+        # return self.__str__()
 
     def die(self):
-        '''
-        method that passes is_alive from True to False
-        '''
-        # try:
-        #     self.is_alive = not self.is_alive
-        # except Exception as e:
-        #     print(e)
+        '''method that passes is_alive from True to False'''
+
         self.is_alive = False
 
-    #     # decorator
-    # def create_lannister(your code here):
-    #     '''
-    #     Method to create characters in a chain
-    #     '''
-    #     #your code here
+        # decorator
+    @classmethod
+    def create_lannister(cls, name, is_alive=True):
+        '''Method to create characters in a chain'''
+
+        return cls(name, is_alive=True)
 
 
 # __str__ et __repr__ :
 # The __str__() and __repr__() methods can be helpful in debugging Python code by logging or printing useful information about an object
 # https://www.digitalocean.com/community/tutorials/python-str-repr-functions
 # https://www.docstring.fr/glossaire/str/
+
+# @classmethod :
+# syntax >
+# @classmethod
+# def func(cls, args...)
+# https://www.programiz.com/python-programming/methods/built-in/classmethod
--- M03/ex01/S1E9.py
+++ M03/ex01/S1E9.py
@@ -10,41 +10,30 @@ with a method that passes is_alive from True to False
     """
 
     def __init__(self, name, is_alive=True):
-        '''
-Init Character's class
-        '''
+        '''Init Character's class'''
 
-        self.name = name
+        self.first_name = name
         self.is_alive = is_alive
 
     @abstractmethod
     def die(self):
-        '''
-method checking if is the character alive
-        '''
+        '''method checking if is the character alive'''
         pass
 
 
 class Stark(Character):
-    """
-"stark" class inherits from Character's class
-    """
+    """"stark" class inherits from Character's class"""
 
     def __init__(self, name, is_alive=True):
-        '''
-Init Stark's class
-        '''
+        '''Init Stark's class'''
         # try:
         #     Character.__init__(self, name, is_alive)
         # except Exception as e:
         #     print(e)
         Character.__init__(self, name, is_alive)
-        self.family_name = "Baratheon"
 
     def die(self):
-        '''
-method that passes is_alive from True to False
-        '''
+        '''method that passes is_alive from True to False'''
         # try:
         #     self.is_alive = False
         # except Exception as e:
