3555711e9da40884ea7d78c98eacd0e478438472
Astrid GAULTIER <asgaulti@bess-f1r5s3.clusters.42paris.fr>
2023-12-10 16:49:18 +0100

    M03 ex02 init

---
+++ M03/ex02/S1E7.py
@@ -0,0 +1,68 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    '''Representing the Baratheon family.'''
+
+    def __init__(self, name, is_alive=True):
+        '''Init Stark's class'''
+
+        Character.__init__(self, name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def __str__(self):
+        return f'Vector: (\'{self.family_name}\', {self.eyes}\',{self.hairs})'
+
+    def __repr__(self):
+        return f'Vector: (\'{self.family_name}\', {self.eyes}\',{self.hairs})'
+        # return self.__str__()
+
+    def die(self):
+        '''method that passes is_alive from True to False'''
+
+        self.is_alive = False
+
+
+class Lannister(Character):
+    '''Representing the Lannister family.'''
+
+    def __init__(self, name, is_alive=True):
+        '''Init Stark's class'''
+
+        Character.__init__(self, name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def __str__(self):
+        return f'Vector: (\'{self.family_name}\', {self.eyes}\',{self.hairs})'
+
+    def __repr__(self):
+        return f'Vector: (\'{self.family_name}\', {self.eyes}\',{self.hairs})'
+        # return self.__str__()
+
+    def die(self):
+        '''method that passes is_alive from True to False'''
+
+        self.is_alive = False
+
+        # decorator
+    @classmethod
+    def create_lannister(cls, name, is_alive=True):
+        '''Method to create characters in a chain'''
+
+        return cls(name, is_alive=True)
+
+
+# __str__ et __repr__ :
+# The __str__() and __repr__() methods can be helpful in debugging Python code by logging or printing useful information about an object
+# https://www.digitalocean.com/community/tutorials/python-str-repr-functions
+# https://www.docstring.fr/glossaire/str/
+
+# @classmethod :
+# syntax >
+# @classmethod
+# def func(cls, args...)
+# https://www.programiz.com/python-programming/methods/built-in/classmethod
