b1be079e063cba169c361f481f81b5d02da37e25
Astrid GAULTIER <asgaulti@bess-f1r5s5.clusters.42paris.fr>
2023-12-12 12:09:35 +0100

    M03 ex04 done (sans deco)

--- M03/ex04/ft_calculator.py
+++ M03/ex04/ft_calculator.py
@@ -2,59 +2,35 @@ class calculator:
     '''This calculator class that is able to do calculations
     (dot product, addition, subtraction) of 2 vectors.'''
 
-    def __init__(self, v1: list[float], v2:list[float]):
+    def __init__(self, v1: list[float], v2: list[float]):
         '''
         Init calculator's class
         '''
         self.V1 = v1
         self.V2 = v2
-        
+
     # decorator
     def dotproduct(V1: list[float], V2: list[float]) -> None:
         ''' decorator : addition of products of the list elements'''
 
+        print("Dot product is:", sum([V1[i] * V2[i] for i in range(len(V1))]))
 
     # decorator
     def add_vec(V1: list[float], V2: list[float]) -> None:
         ''' decorator : addition of 2 vectors'''
 
-        print("Dot product is:", add())
-    
+        print("Add Vector is:", [float(V1[i]) + float(V2[i])
+                                 for i in range(len(V1))])
 
     # decorator
     def sous_vec(V1: list[float], V2: list[float]) -> None:
         ''' decorator : substraction of 2vectors'''
 
-
-    def __add__(self, object) -> None:
-        ''' addition function : add a scalar value to instance's elements '''
-
-        self.v = [i + object for i in self.v]  # comprehension list
-        print(self.v)
-    
-    def __mul__(self, object) -> None:
-        ''' multiplication function :
-        multiply a scalar value to instance's elements '''
-
-        self.v = [i * object for i in self.v]  # comprehension list
-        print(self.v)
-
-    def __sub__(self, object) -> None:
-        ''' substraction function :
-        substract a scalar value to instance's elements '''
-
-        self.v = [i - object for i in self.v]  # comprehension list
-        print(self.v)
-
-    def __truediv__(self, object) -> None:
-        ''' division function :
-        divide a scalar value to instance's elements'''
-
-        if object == 0:
-            return print("ZeroDivisionError: float division by zero")
-        self.v = [i / object for i in self.v]  # comprehension list
-        print(self.v)
+        print("Sous Vector is:", [float(V1[i]) - float(V2[i])
+                                  for i in range(len(V1))])
 
 
 # https://www.programiz.com/python-programming/decorator
 # > Basically, a decorator takes in a function, adds some functionality and returns it.
+# Cet exercice ne nous permet donc pas de faire des decorators
+# (ou alors j'ai pas compris le prototype donne!)
