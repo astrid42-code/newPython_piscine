a3d9e61a37d7870e7e11b533a47639334d305fca
Astrid GAULTIER <asgaulti@bess-f1r5s7.clusters.42paris.fr>
2023-12-14 17:05:28 +0100

    M04 ex00 1'

---
+++ M04/ex00/statistics.py
@@ -0,0 +1,36 @@
+from typing import Any
+
+
+def ft_statistics(*args: Any, **kwargs: Any) -> None:
+    '''function takes in *args a quantity of unknown number
+    and make the Mean, Median, Quartile (25% and 75%),
+    Standard Deviation and Variance according to the **kwargs ask.'''
+
+    # handling errors :
+    # int min et max?
+    # int only?
+    # tjrs le meme type dans la liste
+
+
+    # print(args)
+    # print(" ", kwargs)
+    if (args and kwargs):
+        # print("ouais")
+        # faire des if dans un premier temps en fct du mot cle des kwargs
+        # puis encapsuler avec un dico/une liste (comme dans M00)
+        VALUES = ["mean", "median", "quartile", "std", "var"]
+        
+        for key, value in kwargs.items():
+            if value not in VALUES:
+                # print("out")
+                pass
+            # print("keys = ", key)
+            # print("values = ", value)
+            
+
+    else:
+        print("ERROR")
+
+
+# *args et **kwargs
+# https://deusyss.developpez.com/tutoriels/Python/args_kwargs/
