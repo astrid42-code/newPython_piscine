846d1192cab527e0c1c5fdd9f5ff4dc75ed8b4ad
Astrid GAULTIER <asgaulti@bess-f1r3s1.clusters.42paris.fr>
2023-12-15 18:22:38 +0100

    M04 ex01 ok

---
+++ M04/ex01/in_out.py
@@ -0,0 +1,27 @@
+def square(x: int | float) -> int | float:
+    '''Function that returns the square of argument'''
+
+    return (x * x)
+
+
+def pow(x: int | float) -> int | float:
+    '''function that returns the Exponentiation of argument by himself'''
+
+    return (x ** x)
+
+
+def outer(x: int | float, function) -> object:
+    '''function that takes as argument a number and a function,
+    it returns an object that when called returns the result
+    of the arguments calculation'''
+
+    count = 0
+
+    def inner() -> float:
+        nonlocal count
+        if count != 0:
+            count = function(count)
+        else:
+            count = function(x)
+        return (count)
+    return inner
