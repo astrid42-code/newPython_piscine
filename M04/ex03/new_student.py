6113b7750fa841f6b20fdff17e3731b1212ad057
Astrid GAULTIER <asgaulti@bess-f1r3s1.clusters.42paris.fr>
2023-12-16 17:24:49 +0100

    M04 ex03 done

--- M04/ex03/new_student.py
+++ M04/ex03/new_student.py
@@ -2,15 +2,56 @@ import random
 import string
 from dataclasses import dataclass, field
 
+
 def generate_id() -> str:
-    '''
-    Dataclass that takes as arguments a name and nickname, set active to True,
-    create the student login, and generate a random ID with the generate_id function.
-    You must not use __str__ , __repr__ in your class
-    The login and id should not be initializable and must return an error.
-    '''
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+    '''generate randomly an ID for each user'''
+
+    return ("".join(random.choices(string.ascii_lowercase, k=15)))
+
 
 @dataclass
 class Student:
-#your code here
+    '''Dataclass that takes as arguments a name and nickname,
+    set active to True, create the student login,
+    and generate a random ID with the generate_id function.
+    You must not use __str__ , __repr__ in your class
+    The login and id should not be initializable and must return an error.'''
+
+    # handling error a faire :
+    # le login et l id ne peuvent pas etre initilise
+    # dans les args envoyes a l instanciation de la classe
+    # cf 2eme test du tester
+
+    name: str
+    surname: str
+    active: bool = True
+
+    login: str = field(init=False)
+    id: str = field(init=False)
+    # init=False
+    # > The login and id should not be initializable and must return an error.
+
+    def __post_init__(self):
+        self.login = f"{self.name[0]}{self.surname}"
+        self.id = generate_id()
+
+    def __format__(self, format_spec):
+        if format_spec == "":
+            return f"Student(name='{self.name}', surname='{self.surname}')"
+        else:
+            return f"Student(name='{self.name}', surname='{self.surname}'\
+                             , active={self.active}, login='{self.login()}'\
+                             , id={self,id}')"
+
+    # pratique mais interdit :
+    # def __repr__(self):
+    #     return f"Student(name={self.name}, surname={self.surname}, active={self.active}, login={self.login}, id={self.id})"
+
+
+# def dataclass (merci Bard)
+# Les dataclasses sont conçues pour être utilisées pour représenter des données simples,
+# telles que les points, les rectangles, les personnes, etc.
+# Elles sont particulièrement utiles pour les données
+# qui doivent être stockées dans une base de données ou échangées avec d autres systèmes.
+# [en resume : avec le tag @dataclass, init et autres seront faits par defaut,
+# sauf si je les definis avant]
--- M04/ex03/tester.py
+++ M04/ex03/tester.py
@@ -1,7 +1,16 @@
 from new_student import Student
 
-student = Student(name = "Edward", surname = "agle")
+student = Student(name="Edward", surname="agle")
 print(student)
 
 # Expected output: (id is random)
-# Student(name='Edward', surname='agle', active=True, login='Eagle', id='trannxhndgtolvh')
+# Student(name='Edward', surname='agle', active=True,
+#   login='Eagle', id='trannxhndgtolvh')
+
+student = Student(name="Edward", surname="agle", id="toto")
+print(student)
+
+# Expected output:
+# $> python tester.py
+# ...
+# TypeError: Student.__init__() got an unexpected keyword argument 'id
